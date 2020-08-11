0fcfb3d5fe5569df9593521912544815ca4ed758
jirawan.bn <jirawan.bn@rmuti.ac.th>
2020-08-11 22:36:32 +0700

    6.2-4-Step 4: Add data binding for the view model

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -54,6 +54,8 @@ class SleepTrackerFragment : Fragment() {
                 ViewModelProvider(
                         this, viewModelFactory).get(SleepTrackerViewModel::class.java)
 
+        binding.setLifecycleOwner(this)
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
         return binding.root
     }
 }
